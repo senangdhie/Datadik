c9166178386f0419140a5a1ecc078f6f395496df
FujitsuS936 <senang.dhie@gmail.com>
2019-04-06 16:32:08 +0700

    060419

--- views/global-meta.html.php
+++ views/global-meta.html.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="description" content="Data pokok pendidikan"><meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"><title><?php echo $this->htmlEncode($this->get('pageTitle')).' - '.$this->htmlEncode($this->app->getConfig('siteName')); ?></title>
 <link type="text/css" rel="stylesheet" href="/assets/css/bootstrap.css"><link type="text/css" rel="stylesheet" href="/assets/css/custom.min.css">
 
-<script type="text/javascript" src="/assets/js/jquery.min.js"></script><script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/assets/js/jquery.min.js"><script type="text/javascript" src="/assets/js/popper.min.js"></script><script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
 
 </head><body><?php
 		if($_SERVER['SERVER_NAME']!='localhost' && $_SERVER['HTTPS']!='on') {
